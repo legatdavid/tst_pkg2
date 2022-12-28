d7013db570313a4a5743805f8df127e000c9f2f3
legatdavid <legatdavid>
2022-12-28 08:16:57 +0000

    setup.py zmenen
    doplněn modul tst.mod

--- setup.py.py
+++ setup.py.py
@@ -6,7 +6,8 @@ setup(
     author="legatdavid",
     description="Testing DBC libraries",
     name="tst_pkg2",
+    packages=find_packages(include=["tst_pkg2", "tst_pkg2.*"]),
     version="0.1.0",
-    install_requires=['numpy>=1.10', 'pandas'],
+    install_requires=['numpy'],
     python_requires=">=3.5",
 )
